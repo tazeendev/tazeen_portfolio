fc4ee6b400fd3c4b0d51ada8daaa563dc4285af5
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-14 01:52:47 -0700

    ......

---
+++ lib/controllers/utills/app_colors/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static const Color primary = Color(0xFF0A192F);     // Dark Navy
+  static const Color secondary = Color(0xFF64FFDA);   // Accent Teal
+  static const Color background = Color(0xFF112240);  // Slightly lighter dark
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color heading = Color(0xFFCCD6F6);
+  static const Color subText = Color(0xFF8892B0);
+  static const Color highlight = Color(0xFF64FFDA);   // Same as secondary
+  static const Color linkHover = Color(0xFF52C7B8);
+  static const Color card = Color(0xFF172A45);
+  static const Color button = Color(0xFF233554);
+  static const Color buttonText = Color(0xFF64FFDA);
+  static const Color error = Colors.redAccent;
+}
