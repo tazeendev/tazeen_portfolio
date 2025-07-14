5d18c7e6df49523437fe881d3a149767a00dd4b2
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-14 01:32:27 -0700

    creating mobile tablet and desktop files for responsiveness

--- lib/views/screen_views/home_screen/main_home_screen/main_home_screen.dart
+++ lib/views/screen_views/home_screen/main_home_screen/main_home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tazeen_portfolio/controllers/constants/components/responsive_layout_widegt.dart';
+import 'package:tazeen_portfolio/views/screen_views/home_screen/desktop_home_screen/desktop_home_screen.dart';
+import 'package:tazeen_portfolio/views/screen_views/home_screen/mobile_home_screen/mobile_home_screen.dart';
+import 'package:tazeen_portfolio/views/screen_views/home_screen/tablet_home_screen/tablet_home_screen.dart';
 class MainHomeScreen extends StatelessWidget {
   const MainHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveLayout(desktop: , mobile: mobile, tablet: tablet);
+    return ResponsiveLayout(desktop: DesktopHomeScreen(),
+        mobile: MobileHomeScreen(), tablet: TabletHomeScreen());
   }
 }
